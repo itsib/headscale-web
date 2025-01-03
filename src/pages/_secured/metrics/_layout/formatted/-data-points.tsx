c8a305674c2be4f754311f82cd51923159aa7600
Sergey Krainov <itsib.su@gmail.com>
2025-01-04 00:37:16 +0700

    Some updates

--- src/pages/_secured/metrics/_layout/formatted/-data-points.tsx
+++ src/pages/_secured/metrics/_layout/formatted/-data-points.tsx
@@ -1,21 +1,20 @@
 import { FC, useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-import { DataPoint, MetricType } from '../../../../../types';
+import { DataPoint, MetricType, MetricUnit } from '../../../../../types';
 import { FormattedMetric } from '../../../../../components/formatters/formatted-metric.tsx';
+import { d3BarChart } from '../../../../../utils/d3-bar-chart.ts';
 
 export interface DataPointDisplayProps {
   id: string;
   type: MetricType;
+  unit?: MetricUnit;
   dataPoints: DataPoint[];
 }
 
-export const DataPoints: FC<DataPointDisplayProps> = ({ id, type, dataPoints }) => {
+export const DataPoints: FC<DataPointDisplayProps> = ({ id, type, unit, dataPoints }) => {
   useEffect(() => {
     // if (dataPoints.length > 1) {
     //   console.log(id, type, dataPoints)
     // }
-
-
   }, [id, dataPoints, type]);
 
   function isBarChart(dataPoints: DataPoint[]) {
@@ -38,7 +37,7 @@ export const DataPoints: FC<DataPointDisplayProps> = ({ id, type, dataPoints }) 
   return dataPoints.length === 1 ? (
     <DataPointsSingle {...dataPoints[0]} />
   ) : isBarChart(dataPoints) ? (
-    <DataPointsBarChart dataPoints={dataPoints} />
+    <DataPointsBarChart unit={unit} dataPoints={dataPoints} />
   ) : null;
 }
 
@@ -60,90 +59,31 @@ function DataPointsSingle({ value, unit, attributes }: DataPoint) {
   );
 }
 
-function DataPointsBarChart({ dataPoints }: { dataPoints: DataPoint[] }) {
+function DataPointsBarChart({ dataPoints, unit }: { dataPoints: DataPoint[]; unit?: MetricUnit }) {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const container = ref.current;
     if (!container) return;
 
-    const bars: { value: number, bar: number }[] = [];
-    let maxValue = 0
+    const bars: { bar: string, value: number }[] = [];
+    let barLabel: string | undefined = undefined;
 
     for (let i = 0; i < dataPoints.length; i += 1) {
       const dataPoint = dataPoints[i];
       if (dataPoint.kind !== 'value' || !dataPoint.attributes.length) continue;
 
-      const bar = parseFloat(dataPoint.attributes[0].value as string);
-      const value = parseFloat(dataPoint.value as string);
-      maxValue = Math.max(maxValue, value);
+      const attr = dataPoint.attributes[0];
+      const bar = `${attr.value}`;
 
-      bars.push({ value, bar });
+      const value = parseFloat(dataPoint.value as string);
+
+      bars.push({ bar, value });
+      barLabel = attr.name;
     }
 
-    const width = 200;
-    const height = 140;
-
-    const svg = d3.select(container)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('viewBox', [0, 0, width, height]);
-
-    const scaleX = d3.scaleLinear()
-      .domain([0, bars.length])
-      .range([0, width]);
-
-    const scaleY = d3.scaleLinear()
-      .domain([0, maxValue])
-      .range([height, 0]);
-
-    const barWidth = Math.floor(width / bars.length)
-
-    svg.append('g')
-      .selectAll()
-      .data(bars)
-      .join(enter => {
-        return enter.append('g')
-          .call(g => g.append('rect')
-            .attr('fill', 'transparent')
-            .attr('x', (_d, index) => scaleX(index))
-            .attr('width', barWidth - 1)
-            .attr('y', scaleY(maxValue))
-            .attr('height', scaleY(0))
-          )
-          .call(g => g.append('rect')
-            .attr('data-index', (_d, index) => `${index}`)
-            .attr('fill', 'rgba(var(--text-primary) / 0.8)')
-            .attr('x', (_d, index) => scaleX(index))
-            .attr('width', () => barWidth - 1)
-            .attr('y', (d) => scaleY(d.value))
-            .attr('height', (d) => scaleY(0) - scaleY(d.value))
-          )
-          .call(g => g.on('mouseenter', (e) => {
-            d3
-              .select(e.currentTarget)
-              .selectChild((_, i) => i === 1)
-              .attr('fill', 'rgba(var(--accent) / 0.8)');
-            })
-          )
-          .call(g => g.on('mouseout', (e) => {
-            d3
-              .select(e.currentTarget)
-              .selectChild((_, i) => i === 1)
-              .attr('fill', 'rgba(var(--text-primary))');
-            })
-          );
-      });
-
-    d3.axisLeft(scaleX)
-
-    return () => {
-      if (container) {
-        container.innerHTML = ''
-      }
-    };
-  }, [dataPoints]);
+    return d3BarChart(container, { bars, barLabel, valueLabel: unit });
+  }, [dataPoints, unit]);
 
   return (
     <div className="bar-chart" ref={ref} />
