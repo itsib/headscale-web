e64151923a230ab17bd30f55ae424a5c3fce90a9
Sergey Krainov <itsib.su@gmail.com>
2025-06-11 17:19:26 +0700

    Fixed test

--- src/utils/format-date.spec.ts
+++ src/utils/format-date.spec.ts
@@ -5,7 +5,7 @@ describe('utils/format-date.ts', () => {
   test('#parseDate', () => {
     expect(parseDate('')).toBeNull();
     expect(parseDate('2052-08-22T08:29:10.679837672Z')).toStrictEqual(new Date(2607928150679));
-    expect(parseDate('2052-08-22T08:29:10')).toStrictEqual(new Date(2607902950000));
+    expect(parseDate('2052-08-22T01:29:10.000Z')).toStrictEqual(new Date(2607902950000));
     expect(parseDate('2607902950')).toStrictEqual(new Date(2607902950000));
     expect(parseDate('2607902950000')).toStrictEqual(new Date(2607902950000));
     expect(parseDate(2607902950)).toStrictEqual(new Date(2607902950000));
